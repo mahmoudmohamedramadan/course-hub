fe05f7c40ee70f8d87e9f0e2def67efeb4efb770
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2026-04-17 18:10:36 +0200

    feat: add User resource with listing and viewing capabilities, including user information schema and table configuration

---
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->toggleable(),
+                TextColumn::make('email')
+                    ->label('Email address')
+                    ->searchable()
+                    ->toggleable(),
+                TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('email_verified_status')
+                    ->label('Email verified status')
+                    ->options([
+                        'verified'   => 'Verified',
+                        'unverified' => 'Unverified',
+                    ])
+                    ->query(function ($query, $state) {
+                        return match ($state['value']) {
+                            'verified'   => $query->whereNotNull('email_verified_at'),
+                            'unverified' => $query->whereNull('email_verified_at'),
+                            default      => $query,
+                        };
+                    }),
+            ], FiltersLayout::Modal)
+            ->recordActions([
+                ViewAction::make(),
+            ])
+            ->toolbarActions([
+                //
+            ]);
+    }
+}
