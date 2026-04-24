d694e7758ed20ee7f31a150a4edbe3dfa4e83772
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2026-04-24 17:41:11 +0300

    refactor: remove CreateAction from ListUsers page to streamline header actions

--- app/Filament/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -2,9 +2,8 @@
 
 namespace App\Filament\Resources\Users\Pages;
 
-use App\Filament\Resources\Users\UserResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\Users\UserResource;
 
 class ListUsers extends ListRecords
 {
@@ -13,7 +12,7 @@ class ListUsers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            //
         ];
     }
 }
